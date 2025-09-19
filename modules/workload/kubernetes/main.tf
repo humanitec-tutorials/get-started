277ab85f4222d9c0de876899b786c9cada3ea9d9
Tobias Babin <tobias.babin@humanitec.com>
2025-09-19 14:37:37 +0200

    Added simple workload module

---
+++ modules/workload/kubernetes/main.tf
@@ -0,0 +1,38 @@
+# Module for creating a simple default Kubernetes workload based on a Deployment
+
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2"
+    }
+  }
+}
+
+# Minimalistic Kubernetes deployment
+resource "kubernetes_deployment" "simple" {
+  metadata {
+    name      = var.name
+    namespace = var.namespace
+  }
+  spec {
+    selector {
+      match_labels = {
+        app = var.name
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = var.name
+        }
+      }
+      spec {
+        container {
+          image = var.image
+          name  = "main"
+        }
+      }
+    }
+  }
+}
---
+++ modules/workload/kubernetes/variables.tf
@@ -0,0 +1,9 @@
+variable "name" {
+  description = "The name of the workload"
+}
+variable "namespace" {
+  description = "Name of the namespace where to create the workload"
+}
+variable "image" {
+  description = "Image to use for the workload"
+}
