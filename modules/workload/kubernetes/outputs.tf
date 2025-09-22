8fe3216e9a08273458dadf04d89a72e5cc8bf4aa
Tobias Babin <tobias.babin@humanitec.com>
2025-09-22 12:01:07 +0200

    Added metadata output to module

--- modules/workload/kubernetes/outputs.tf
+++ modules/workload/kubernetes/outputs.tf
@@ -1,1 +1,14 @@
-# TODO - provide metadata
+output "humanitec_metadata" {
+  description = "Metadata for Humanitec."
+  value = merge(
+    {
+      "Namespace" = var.namespace
+    },
+    {
+      "Image" = var.image
+    },
+    {
+      "Deployment" = kubernetes_deployment.simple.metadata[0].name
+    }
+  )
+}
