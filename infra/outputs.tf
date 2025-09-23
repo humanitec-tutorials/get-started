5dea6d80f7f852365fa997ef0355ab42f22ec9f0
Tobias Babin <tobias.babin@humanitec.com>
2025-09-23 12:48:59 +0200

    Added enabled cloud provider to output

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,3 +1,6 @@
+output "enabled_cloud_provider" {
+  value = var.enabled_cloud_provider
+}
 output "k8s_connect_command" {
   value = length(aws_eks_cluster.get-started) > 0 ? "aws eks update-kubeconfig --region ${var.aws_region} --name ${aws_eks_cluster.get-started[0].name}" : "No cluster created"
 }
