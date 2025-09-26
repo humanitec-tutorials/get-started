a4aad04451a3c62734b25b05952e24b7ae9f3cb3
Tobias Babin <tobias.babin@humanitec.com>
2025-09-26 12:58:49 +0200

    Created new outputs and variables to provide data from intra

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -4,3 +4,16 @@ output "enabled_cloud_provider" {
 output "k8s_connect_command" {
   value = length(aws_eks_cluster.get-started) > 0 ? "aws eks update-kubeconfig --region ${var.aws_region} --name ${aws_eks_cluster.get-started[0].name}" : "No cluster created"
 }
+output "k8s_cluster_name" {
+  value = aws_eks_cluster.get-started[0].name
+}
+output "k8s_cluster_endpoint" {
+  value = aws_eks_cluster.get-started[0].endpoint
+}
+output "cluster_ca_certificate" {
+  value     = aws_eks_cluster.get-started[0].certificate_authority[0].data
+  sensitive = true
+}
+output "agent_runner_irsa_role_arn" {
+  value = aws_iam_role.agent_runner_irsa_role.arn
+}
--- orchestrator/variables.tf
+++ orchestrator/variables.tf
@@ -16,3 +16,15 @@ variable "orchestrator_org" {
 variable "orchestrator_auth_token" {
   description = "Orchestrator auth token"
 }
+variable "k8s_cluster_name" {
+  description = "Name of the runner K8s cluster"
+}
+variable "k8s_cluster_endpoint" {
+  description = "Endpoing of the runner K8s cluster API server"
+}
+variable "cluster_ca_certificate" {
+  description = "CA cert of the runner K8s cluster"
+}
+variable "agent_runner_irsa_role_arn" {
+  description = "AWS IAM Role arn for the agent runner"
+}
